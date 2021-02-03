cd075bce1e5f435e382f318f634c6064bbc3818a
choi <boy0516@naver.com>
2021-02-03 17:09:03 +0900

    'heroku'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 // http://127.0.0.1:3000/
 app.get('/', (req, res) => res.send('Hello, Nodejs!'))
 // http://127.0.0.1:3000/welcom?name=choi
-app.get('/welcome',(req, res)=>{
+app.get('/service',(req, res)=>{
     var user_status = req.param('status')
     //res.send('Hi,ther.welcome to the nodejs. [' + user_status +')')
     res.send('The system is running [Status: : ON]')
