7a5748e4337e0fbb221e4f3a8ec706233a562fd2
choi <boy0516@naver.com>
2021-02-03 17:00:57 +0900

    'heroku'

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+// http://127.0.0.1:3000/
+app.get('/', (req, res) => res.send('Hello, Nodejs!'))
+// http://127.0.0.1:3000/welcom?name=choi
+app.get('/welcome',(req, res)=>{
+    var user_status = req.param('status')
+    //res.send('Hi,ther.welcome to the nodejs. [' + user_status +')')
+    res.send('The system is running [Status: : ON]')
+})
+app.get('/check',(req, res)=>res.send('Example app listen on port 3000'))
+app.listen(port, () => console.log('Example app listen on port 3000'))
