0c07e5b9618b6fde22465cedd284b2ba78894472
choi <boy0516@naver.com>
2021-02-03 17:13:41 +0900

    heroku

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 // http://127.0.0.1:3000/
-app.get('/', (req, res) => res.send('Hello, Nodejs!'))
-// http://127.0.0.1:3000/welcom?name=choi
+app.get('/', (req, res) => res.send('과제 페이지는 https://choijihun-exam-api.herokuapp.com/service?status=on 입니다'))
+// http://127.0.0.1:3000/service?status=on
 app.get('/service',(req, res)=>{
     var user_status = req.param('status')
     //res.send('Hi,ther.welcome to the nodejs. [' + user_status +')')
-    res.send('The system is running [Status: : ON]')
+    res.send('The system is running [Status:'+ user_status.toUpperCase+']')
 })
 app.get('/check',(req, res)=>res.send('Example app listen on port 3000'))
 app.listen(port, () => console.log('Example app listen on port 3000'))
